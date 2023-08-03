ac59eac9d495063e8d1773ae1a0584ad427f7f30
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-08-03 18:01:42 +0100

    change

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -1,27 +1,35 @@
 ﻿public class Program
 {
-    //Array it allows storing of multiple values in a variable
 
-    int v = 56; //single value
-    static int[] y = { 675, 6754, 90, 869, 674, 74, 867, 8, 1, 32, 4, 4, 5 }; //Array
+    //operators
 
-    static string[] words = { "chalk", "biro", "house", "bin", "pencil", "board" };
+    //arithmetic oprators +,-,*,/,%
+    //logical operators
+    //assignment operators
+    //relations operators
 
-    
+
+    // arithmetic operator
+
+    static decimal v = 78; //field
+    static decimal x = 90;
+    static decimal r = x * v;
+    static decimal n = x + v;
+    static decimal o = x / v;
+    static decimal a = x - v;
+
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome to c# class");
+        Console.WriteLine("my name is namecannotbeblank");
+        Console.WriteLine( "******************");
+        Console.WriteLine(r);
+        Console.WriteLine(n);
+        Console.WriteLine(o);
+        Console.WriteLine(a);
+        Console.WriteLine("******************");
 
-        foreach( int i in y)
-        {
-            Console.WriteLine(i);
-        }
 
-        foreach(string data in words)
-        {
-            Console.WriteLine(data);
-        }
 
     }
 
