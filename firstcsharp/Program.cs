34d0e3c8b423b45529b4afed377c62318699b4a8
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-08-04 17:25:26 +0100

    changes

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -1,34 +1,61 @@
-﻿public class Program
+﻿
+public class Program
 {
 
     //operators
 
-    //arithmetic oprators +,-,*,/,%
-    //logical operators
-    //assignment operators
-    //relations operators
+    //arithmetic operators +,-,*,/,%
+    //logical operators &&,||,!,&,|
+    //assignment operators =,+=,-=,*=,/=, <<
+    //relations operators > greater than,< lessser than,==,!=,>=,<=
 
 
     // arithmetic operator
 
-    static decimal v = 78; //field
+    static decimal v = 90; //field
     static decimal x = 90;
-    static decimal r = x * v;
-    static decimal n = x + v;
-    static decimal o = x / v;
-    static decimal a = x - v;
+    static decimal r = x += v;
+    static decimal n = x -= v;
+    static decimal o = x /= v;
+    static decimal a = x *= v;
 
+    int two = 10;
+    int one = 16;
+   
+   
+
+    static string words = "Bookshop";
+    static string values = "calculator";
+    static string result = "Bookshop";
+
+   
+
+ 
 
     private static void Main(string[] args)
     {
-        Console.WriteLine("my name is namecannotbeblank");
-        Console.WriteLine( "******************");
         Console.WriteLine(r);
         Console.WriteLine(n);
         Console.WriteLine(o);
         Console.WriteLine(a);
         Console.WriteLine("******************");
 
+        if(v >= x )
+        {
+            Console.WriteLine(" yes they match");
+        }
+        else
+        {
+            Console.WriteLine("they dont match");
+        }
+        if(v <= x)
+        {
+            Console.WriteLine("they are correct");
+        }
+        else
+        {
+            Console.WriteLine("they are not correct");
+        }
 
 
     }
