942fde90c1f7f784df88ef408ada7934a4742268
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-08-17 17:00:45 +0100

    changes

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -1,50 +1,114 @@
 ﻿using firstcsharp.Topics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-
+public delegate void Print(int value);
 public class Program
 {
+    //Icollection,Ilist
+    //dictionary
+    //list,stack,qeue and set
 
     private static void Main(string[] args) //method or behaviour or logic or function
     {
-       List<Car> carmodel = new List<Car>();
-        carmodel.Add(new Car() { Id = 101, Name="Benz", Model=2017, Price = 2000000 });
-        carmodel.Add(new Car() { Id = 102, Name = "Camry", Model = 2018, Price = 1000000 });
-        carmodel.Add(new Car() { Id = 103, Name = "Peguot", Model = 2013, Price = 6700000 });
-        carmodel.Add(new Car() { Id = 104, Name = "Toyota", Model = 2014, Price = 3000000 });
-        carmodel.Add(new Car() { Id = 105, Name = "Bugatti", Model = 2022, Price = 5000000 });
-        carmodel.Add(new Car() { Id = 106, Name = "Lexus", Model = 2012, Price = 2300000 });
+        Car car1 = new Car()
+        {
+            Id = 101,
+            Name = "Honda",
+            Price = 5000000,
+            Model = 2015,
+        };
 
-        outdateddelgate outdated = new outdateddelgate(Carfiltering);
-        Car.OldCars(carmodel,outdated);
-        Console.WriteLine( "-----------");
-        outdateddelgate outdateds = new outdateddelgate(Carfilter);
-        Car.OldCars(carmodel, outdateds);
+        Car car2 = new Car()
+        {
+            Id = 102,
+            Name = "Benz",
+            Price = 6000000,
+            Model = 2018,
+        };
 
-        Console.ReadKey();
+        Car car3 = new Car()
+        {
+            Id = 103,
+            Name = "Toyota",
+            Price = 2000000,
+            Model = 2001,
+        };
+
+        Car car4 = new Car()
+        {
+            Id = 104,
+            Name = "Camry",
+            Price = 1500000,
+            Model = 2011,
+        };
+
+        Car car5 = new Car()
+        {
+            Id = 105,
+            Name = "Bugatti",
+            Price = 2500000,
+            Model = 2015,
+        };
+
+        List<Car> cars = new List<Car>(3);
+        cars.Add(car1);
+        cars.Add(car2);
+        cars.Add(car3);
+        cars.Add(car4);
+
+        //Car liscar = cars[3];
+        foreach(var liscar in cars)
+        {
+            Console.WriteLine("Id = {0},Name={1},Price ={2},Model={3}", liscar.Id, liscar.Name, liscar.Price, liscar.Model);
+        }
+        Console.WriteLine("************");
+        Car c = cars.Find(x => x.Price <3000000);
+        Console.WriteLine("Id = {0},Name={1},Price ={2},Model={3}", c.Id, c.Name, c.Price, c.Model);
+
+
+
+
+
+        //List<string> list = new List<string>();
+        //list.Add("Chalk");
+        //list.Add("Tyron");
+        //list.Add("Clone");
+        //list.Add("Reserve");
+        //list.Add("Allign");
+        //list.Add("Disregard");
+
+        //collectiondisplay(list);
+        //ilistdisplay(list); 
+        //listdisplay(list);
+
     }
 
-    public static bool Carfiltering(Car car)
-    {
-        if(car.Price < 2300000)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    public static bool Carfilter(Car car)
-    {
-        if (car.Id < 104)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    //public static void collectiondisplay(ICollection<string> mylist)
+    //{
+    //    mylist.Add("charge");
+    //    foreach (string item in mylist)
+    //    {
+    //        Console.WriteLine("the icollection elemets are as follows:" + item);
+    //    }
+    //}
+    //public static void ilistdisplay(IList<string> mylist)
+    //{
+    //    mylist.Add("data");
+    //    foreach (string item in mylist)
+    //    {
+    //        Console.WriteLine("the ilist elemets are as follows:" + item);
+    //    }
+    //}
+    //public static void listdisplay(List<string> mylist)
+    //{
+    //    mylist.Sort();
+    //    foreach (string item in mylist)
+    //    {
+    //        Console.WriteLine("the list elemets are as follows:" + item);
+    //    }
+    //}
+
+
 
 
 }
--- firstcsharp/Topics/Car.cs
+++ firstcsharp/Topics/Car.cs
@@ -17,18 +17,6 @@ namespace firstcsharp.Topics
         public int Price { get; set; }
         public int Model { get; set; }
 
-
-        public static void OldCars(List<Car> cars, outdateddelgate outdated)
-        {
-            foreach (Car car in cars)
-            {
-                if(outdated(car))
-                {
-                    Console.WriteLine(car.Name +"-------" +"Outdated");
-                }
-            }
-        }
-        
     }
 
    
